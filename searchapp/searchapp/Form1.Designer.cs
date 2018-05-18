664645f353cdb4626fdec78b8f13e1781bb31dfa
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-05-18 12:29:51 -0400

    Listview search app using C#

---
+++ searchapp/searchapp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace searchapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtNewSearch = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 41);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(476, 295);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "First Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Last Name";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Age";
+            this.columnHeader3.Width = 150;
+            // 
+            // txtNewSearch
+            // 
+            this.txtNewSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtNewSearch.Name = "txtNewSearch";
+            this.txtNewSearch.Size = new System.Drawing.Size(202, 20);
+            this.txtNewSearch.TabIndex = 2;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(220, 12);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "Search";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 336);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtNewSearch);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox txtNewSearch;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ searchapp/searchapp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace searchapp
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Load_List_View();
+        }
+
+
+        private void Load_List_View()
+        {
+            string[] strItemcoll3 = { "John", "Doe", "30" };
+            var listViewItem = new ListViewItem(strItemcoll3);
+            listView1.Items.Add(listViewItem);
+
+            string[] strItemcoll4 = { "Eric", "Nay", "45" };
+            var listViewIte4 = new ListViewItem(strItemcoll4);
+            listView1.Items.Add(listViewIte4);
+
+            string[] strItemcoll5 = { "Brandon", "goit", "15" };
+            var listViewItem5 = new ListViewItem(strItemcoll5);
+            listView1.Items.Add(listViewItem5);
+
+            string[] strItemcoll6 = { "Ramon", "Askjh", "96" };
+            var listViewItem6 = new ListViewItem(strItemcoll6);
+            listView1.Items.Add(listViewItem6);
+
+            string[] strItemcoll7 = { "Guilherme", "bras", "66" };
+            var listViewItem7 = new ListViewItem(strItemcoll7);
+            listView1.Items.Add(listViewItem7);
+
+            string[] strItemcoll8 = { "Daimian", "smith", "12" };
+            var listViewItem8 = new ListViewItem(strItemcoll8);
+            listView1.Items.Add(listViewItem8);
+        }
+
+
+        //index of last search. 
+        private int index_search = 0;
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //skips if there is no items on listview
+            if (listView1.Items.Count == 0)
+                return;
+
+            //if indext of last seach equals last item on listview, there are no more items. 
+            if (index_search == listView1.Items.Count)
+            {
+                MessageBox.Show("No More Items Found!");
+                index_search = 0;
+            }
+
+            //search function that will search listview. 
+            ListViewItem foundItem =
+                listView1.FindItemWithText(txtNewSearch.Text, true, index_search, true);
+            if (foundItem != null)
+            {
+                //used to scrow to found item. 
+                listView1.TopItem = foundItem;
+                foundItem.Selected = true;
+                listView1.Select();
+                index_search = foundItem.Index + 1;
+
+            }
+            else
+            {
+                //no items found messages. 
+                if (index_search.Equals(0))
+                    MessageBox.Show("Items Not Found");
+                else
+                    MessageBox.Show("No More Items Found!");
+
+                //reset index search to start from top of the list. 
+                index_search = 0;
+            }
+        }
+    }
+}
